77d3456bb40fd202df69eecc522cb19d890e45df
Dan Purdy <dan.purdy@urbanmassage.com>
2017-07-23 00:19:56 +0100

    Add Cache timeout

--- app/shared/cache.js
+++ app/shared/cache.js
@@ -1,6 +1,6 @@
 const rush = require('node-rush');
 
 module.exports = rush({
-  maxAge: process.env.CACHE_TIMEOUT || 900000, // 15 minute cache
+  maxAge: process.env.CACHE_TIMEOUT || 900000, // 15 minute default cache
   errTTL: 5000,
 });
